3ca797f4a1e1c7f21809b661bbbaf179a0c16692
Marine DELPON <marine@mbp-de-marine.home>
2022-10-08 20:13:21 +0200

    show products on homepage done

---
+++ front/js/script.js
@@ -0,0 +1,34 @@
+//récupère les données depuis l'API
+function getProductsFromApi(urlApi) {
+  return fetch(urlApi).then(function (response) {
+    return response.json();
+  });
+}
+
+//affichage des produits
+function showProducts(products) {
+  for (const product of products) {
+    showProduct(product);
+  }
+}
+
+//affichage d'un produit
+function showProduct(product) {
+  let productLink = document.createElement("a");
+  productLink.innerHTML = `
+    <article>
+        <img src="${product.imageUrl}" alt="${product.altTxt}">
+        <h3 class="productName">${product.name}</h3>
+        <p class="productDescription">${product.description}</p>
+    </article>
+    `;
+   document.querySelector('#items').appendChild(productLink);
+}
+
+//point d'entrée du script
+function main() {
+  let url = "http://localhost:3000/api/products";
+  getProductsFromApi(url).then(showProducts);
+}
+
+main();
