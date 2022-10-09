cea8221da758c3e2ee3ec084010103717927ff75
Marine DELPON <marine@mbp-de-marine.home>
2022-10-09 19:47:51 +0200

    cart local storage implementation

--- front/js/product.js
+++ front/js/product.js
@@ -1,6 +1,8 @@
 function main() {
     const id = getIdFromUrl();
     getProductFromApi(id).then(showProductDetails)
+    let btnAddToCart = document.querySelector('#addToCart');
+    btnAddToCart.addEventListener('click', handleAddToCartEvent)
 }
 
 main();
@@ -28,6 +30,7 @@ function showProductDetails(details){
    let description = document.querySelector('#description');
    let colorSelect = document.querySelector('#colors');
 
+   document.title = details.name;
    img.innerHTML = `<img src="${details.imageUrl}" alt="${details.altTxt}">`;
    title.textContent = details.name;
    price.textContent = details.price;
@@ -39,4 +42,44 @@ function showProductDetails(details){
     colorOption.textContent = color;
     colorSelect.appendChild(colorOption);
    }
+}
+
+//recupère la couleur, l'id et la quantité du produit choisit par l'utilisateur
+function handleAddToCartEvent(){
+    let id = getIdFromUrl();
+    let color = document.querySelector('#colors').value;
+    let quantity = parseInt(document.querySelector('#quantity').value);
+    if (color != '' && quantity != 0){
+        addProductToCart(id, color, quantity);
+    }
+}
+
+//créer un panier ou renvoie le panier stocké dans le storage
+function getCartStore(){
+    const cart = localStorage.getItem('cart');
+    if (cart === null){
+        return [];
+    }
+    return JSON.parse(cart);
+}
+
+//stock le panier dans le storage
+function storeCart(cart){
+    localStorage.setItem('cart', JSON.stringify(cart));
+}
+
+//ajoute les données du produit choisit dans le local storage
+function addProductToCart(id, color, quantity){
+    const newProduct = {id, color, quantity};
+    const cart = getCartStore();
+    //recherche si le produit exxiste déjà dans le panier
+    const index = cart.findIndex(function(product){
+        return product.id == newProduct.id && product.color == newProduct.color; //comparaison
+    })
+    if (index == -1){
+        cart.push(newProduct);
+    } else {
+        cart[index].quantity += quantity;
+    }
+    storeCart(cart);
 }
