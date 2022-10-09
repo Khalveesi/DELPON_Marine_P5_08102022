cae5e33ad395c1697eb4aea9833b08e2c06ec490
Marine DELPON <marine@mbp-de-marine.home>
2022-10-09 17:12:51 +0200

    show product's details on product page

--- front/js/product.js
+++ front/js/product.js
@@ -1,5 +1,6 @@
 function main() {
     const id = getIdFromUrl();
+    getProductFromApi(id).then(showProductDetails)
 }
 
 main();
@@ -8,4 +9,35 @@ main();
 function getIdFromUrl(){
     const urlParams = new URLSearchParams(window.location.search);
     return urlParams.get('id');
+}
+
+//récupère un produit depuis l'API avec l'ID
+
+function getProductFromApi(id){
+    return fetch(`http://localhost:3000/api/products/${id}`).then(function(response){
+        return response.json();
+    });
+}
+
+//montre les détails d'un produit
+
+function showProductDetails(details){
+    console.log(details)
+   let img = document.querySelector('.item__img');
+   let title = document.querySelector('#title');
+   let price = document.querySelector('#price');
+   let description = document.querySelector('#description');
+   let colorSelect = document.querySelector('#colors');
+
+   img.innerHTML = `<img src="${details.imageUrl}" alt="${details.altTxt}">`;
+   title.textContent = details.name;
+   price.textContent = details.price;
+   description.textContent = details.description;
+
+   for (let color of details.colors){
+    let colorOption = document.createElement('option');
+    colorOption.value = color.toLowerCase();
+    colorOption.textContent = color;
+    colorSelect.appendChild(colorOption);
+   }
 }
